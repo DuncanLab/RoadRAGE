0c1792aab9ffe8e7ba491a6b6dd44e3df61373ed
Cody Howarth <codyhowarth@gmail.com>
2019-02-17 23:39:43 -0500

    Add - points tracking based on pickup
    - Currently the first road prefab has a few test
    capsules in it.

--- Assets/scripts/GameData.cs
+++ Assets/scripts/GameData.cs
@@ -52,8 +52,21 @@ public class GameData
         public string FileLocation; // Path to image file used for heads up display.
         public float TimeAllotted; // Amount of time in seconds that the trial will continue for. <--- when this is equal to -1 it means they need to press "SPACE" (OM). <---- When this is any positive number that means it remains on the screen for that long(OM)
         public bool resourcesRemain; // Whether or not the user still has resources left, if not we end the trial.
+        public int PointsCollected; // Number of points the user has collected.
+        public List<Pickup> Pickups; // A list of all the pickup values for the trial
         public List<Event> Events; // List of all spawn events for the trial.
         public List<ScoreAssignment> FinalScoreAssignments; //  <--- depending on what lane they end in, they recieve point value associated with lane.
+    }
+
+    [Serializable]
+    public class Pickup
+    {
+        public string PickupName; // The name of the pickup item, this must match the object name in the prefab
+        public double WinChance; // The probability of winning from 0 to 1
+        public double LoseChance; // The probability of losing from 0 to 1
+        public int WinPoints; // Points to be won
+        public int LosePoints; // Points to be lost
+
     }
 
     [Serializable]
--- Assets/scripts/PointsController.cs
+++ Assets/scripts/PointsController.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
+using Random = System.Random;
 
 public class PointsController : MonoBehaviour
 {
 
     public ProgressBar hungerBar;
     public ProgressBar thirstBar;
+    public TextMeshProUGUI pointsCounter;
 
     private bool isFoodCollision = false;
     private bool isDrinkCollision = false;
+    private bool isOnGoingCollision = false;
+
     private int foodPickupOffset = 0;
     private int drinkPickupOffset = 0;
 
@@ -21,6 +26,7 @@ public class PointsController : MonoBehaviour
 
         hungerBar = GameObject.Find("HungerBar").gameObject.GetComponent<ProgressBar>();
         thirstBar = GameObject.Find("ThirstBar").gameObject.GetComponent<ProgressBar>();
+        pointsCounter = GameObject.Find("PointsCounter").gameObject.GetComponent<TextMeshProUGUI>();
 
         // We start with resources
         data.currTrial.resourcesRemain = true;
@@ -29,6 +35,7 @@ public class PointsController : MonoBehaviour
     private void Update()
     {
         UpdateProgressBars();
+        UpdatePointsCounter();
 
         // When either resource gets to zero, end the trial.
         if (hungerBar.BarValue == 0 || thirstBar.BarValue == 0)
@@ -39,16 +46,61 @@ public class PointsController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name.EndsWith("food"))
+        // If we don't do this, points could be counted more than once.
+        if (isOnGoingCollision)
+        {
+            return;
+        }
+        else
+        {
+            isOnGoingCollision = true;
+        }
+
+        if (other.name.ToLower().EndsWith("food"))
         {
             isFoodCollision = true;
         }
-        else if (other.name.EndsWith("drink"))
+        else if (other.name.ToLower().EndsWith("drink"))
         {
             isDrinkCollision = true;
         }
+        else if (other.name.ToLower().Contains("money"))
+        {
+            foreach (GameData.Pickup pickup in data.currTrial.Pickups)
+            {
+                if (other.name.ToLower().Equals(pickup.PickupName.ToLower()))
+                {
+                    // Run the probabilities
+                    int newPoints = DeterminePointsUsingProbabilities(pickup.WinChance, pickup.LoseChance, pickup.WinPoints, pickup.LosePoints);
+                    data.currTrial.PointsCollected += newPoints;
+                    break;
+                }
+            }
+        }
 
         Destroy(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isOnGoingCollision = false;
+    }
+
+    private int DeterminePointsUsingProbabilities(double WinProb, double LoseProb, int WinPoints, int LosePoints)
+    {
+        int points = 0;
+        Random rand = new Random();
+        bool isWin = rand.NextDouble() < WinProb;
+
+        if (isWin)
+        {
+            points = WinPoints;
+        }
+        else
+        {
+            points = LosePoints;
+        }
+        return points;
     }
 
     private void UpdateProgressBars()
@@ -71,5 +123,10 @@ public class PointsController : MonoBehaviour
 
         thirstBar.BarValue = newBarValue;
         thirstBar.BarValue += drinkPickupOffset;
+    }
+
+    private void UpdatePointsCounter()
+    {
+        pointsCounter.text = "Points: " + data.currTrial.PointsCollected;
     }
 }
